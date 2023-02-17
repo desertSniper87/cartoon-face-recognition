9c11d98ef39c339a1eaf2660ce2af29ce0e133ba
Samidhya Sarker <samidhya.sarker@bcc.gov.bd>
2023-02-17 14:18:23 +0600

    baler mac

--- facedetector-training/mtcnn/__init__.py
+++
@@ -1,5 +0,0 @@
-from mtcnn.deploy import get_net, get_net_caffe, get_default_detector
-from mtcnn.deploy.detect import FaceDetector
-from mtcnn.deploy.batch_detect import BatchImageDetector
-from mtcnn.deploy.tracker import FaceTracker
-from mtcnn.deploy.align import align_multi
