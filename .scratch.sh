8f794d188e125eb126f2b531421e5fd98b6a620d
desertsniper87 <torshobuet@gmail.com>
2023-02-20 21:45:41 +0600

    update from e203mah

--- .scratch.sh
+++ .scratch.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash 
 
-FOLDER=retinaface_training
-REPO_URL=git@github.com:desertSniper87/Pytorch_Retinaface.git
+# FOLDER=retinaface_training
+# REPO_URL=git@github.com:desertSniper87/Pytorch_Retinaface.git
 
-git subtree add \
+# git subtree add \
+    # --prefix \
+    # $FOLDER \
+    # $REPO_URL master \
+    # --squash
+
+FOLDER=facedetector_training
+REPO_URL=git@github.com:desertSniper87/FaceDetector-mtcnn.git
+
+git subtree pull \
     --prefix \
     $FOLDER \
-    $REPO_URL master \
-    --squash
+    $REPO_URL master 
 
